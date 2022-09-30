0fce223eb915a07c5f3328b6a6ec8fb1cb68f9f1
BhawitBalodi <Bhawit.Bhawitbalodi@gmail.com>
2022-09-30 22:38:53 +0530

    hh

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tensorflow import keras
 from flask import *
 app = Flask(__name__)
 
-model = tf.keras.models.load_model('NeuralN_try6.h5')
+model = tf.keras.models.load_model('NeuralN_try8.h5')
 
 
 @app.route('/')
@@ -20,12 +20,12 @@ def home():
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     Air_Temp = request.form.get('Air_Temp')
-    Relative_Humidity = request.form.get('Relative_Humidity')
-    Solar_Radiation = request.form.get('Solar_Radiation')
-    RTD = request.form.get('RTD')
+    Wind_speed = request.form.get('Wind_speed')
+    Pannel_temp = request.form.get('Pannel_temp')
     Array_Voltage = request.form.get('Array_Voltage')
     Array_Current = request.form.get('Array_Current')
-    model_input = tf.strings.to_number([Air_Temp, Relative_Humidity, Solar_Radiation, RTD, Array_Voltage, Array_Current])
+    Solar_Radiation = request.form.get('Solar_Radiation')
+    model_input = tf.strings.to_number([Air_Temp,Wind_speed, Pannel_temp,Array_Voltage,Array_Current,Solar_Radiation])
     model_input = tf.expand_dims(model_input, axis=0)
     # model_input = tf.expand_dims(model_input, axis=1)
     result = model.predict(model_input)
